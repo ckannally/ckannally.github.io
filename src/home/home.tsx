87c9dbb677f353a9a279b2e69d41d5554647063b
Connor Kannally <44526884+ckannally@users.noreply.github.com>
2024-05-16 22:03:18 -0400

    More for initial homepage

---
+++ src/home/home.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import './home.css'
+
+function Header () {
+
+    return (
+            <div id="top">
+                <div id="links">
+                    <h4>About Me</h4>
+                    <h4>Curriculum Vitae & Research</h4>
+                    <h4>Me-n-Luna</h4>
+                    <h4>Cooking</h4>
+                    <h4>Backcountry</h4>
+                    <h4>Horticulture</h4>
+                    <h4>Fly-Fishing</h4>
+                    <h4>Skiing</h4>
+                </div>
+            </div>
+    )
+
+}
+export default Header
